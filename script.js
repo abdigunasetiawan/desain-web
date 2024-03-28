dc23b4aab985479a31d7318168c66e55e64618e6
abdigunasetiawan <abdigunasetiawan0@gmail.com>
2024-03-28 07:53:33 +0700

    menambahkan folder pertemuan4

---
+++ script.js
@@ -0,0 +1,258 @@
+window.addEventListener('load', () => {
+  if (!localStorage.getItem('showOnBoarding')) {
+    localStorage.setItem('showOnBoarding', true);
+    onBoardingHandler();
+  } else {
+    onBoardingHandler();
+  }
+});
+
+const containerContent = `<div class="container mx-auto px-4">
+<div class="flex justify-between">
+  <table class="text-slate-800 dark:text-slate-300">
+    <tr>
+      <td>Anggota 1</td>
+      <td>:</td>
+      <td>Abdi Guna Setiawan</td>
+    </tr>
+    <tr>
+      <td>NPM</td>
+      <td>:</td>
+      <td>23441001</td>
+    </tr>
+    <tr>
+      <td>Anggota 2</td>
+      <td>:</td>
+      <td>Fachrizal Hadiansyah</td>
+    </tr>
+    <tr>
+      <td>NPM</td>
+      <td>:</td>
+      <td>23441186</td>
+    </tr>
+    <tr>
+      <td>Program Studi</td>
+      <td>:</td>
+      <td>Sistem Informasi</td>
+    </tr>
+    <tr>
+      <td>Kelas</td>
+      <td>:</td>
+      <td>A (Pagi)</td>
+    </tr>
+  </table>
+
+  <div class="hidden md:flex gap-2">
+    <svg class="size-10 " xmlns="http://www.w3.org/2000/svg" width="2500" height="2432" viewBox="0 0 256 249" preserveAspectRatio="xMinYMin meet">
+      <g fill="#161614" class="dark:fill-slate-200">
+        <path d="M127.505 0C57.095 0 0 57.085 0 127.505c0 56.336 36.534 104.13 87.196 120.99 6.372 1.18 8.712-2.766 8.712-6.134 0-3.04-.119-13.085-.173-23.739-35.473 7.713-42.958-15.044-42.958-15.044-5.8-14.738-14.157-18.656-14.157-18.656-11.568-7.914.872-7.752.872-7.752 12.804.9 19.546 13.14 19.546 13.14 11.372 19.493 29.828 13.857 37.104 10.6 1.144-8.242 4.449-13.866 8.095-17.05-28.32-3.225-58.092-14.158-58.092-63.014 0-13.92 4.981-25.295 13.138-34.224-1.324-3.212-5.688-16.18 1.235-33.743 0 0 10.707-3.427 35.073 13.07 10.17-2.826 21.078-4.242 31.914-4.29 10.836.048 21.752 1.464 31.942 4.29 24.337-16.497 35.029-13.07 35.029-13.07 6.94 17.563 2.574 30.531 1.25 33.743 8.175 8.929 13.122 20.303 13.122 34.224 0 48.972-29.828 59.756-58.22 62.912 4.573 3.957 8.648 11.717 8.648 23.612 0 17.06-.148 30.791-.148 34.991 0 3.393 2.295 7.369 8.759 6.117 50.634-16.879 87.122-64.656 87.122-120.973C255.009 57.085 197.922 0 127.505 0" />
+        <path d="M47.755 181.634c-.28.633-1.278.823-2.185.389-.925-.416-1.445-1.28-1.145-1.916.275-.652 1.273-.834 2.196-.396.927.415 1.455 1.287 1.134 1.923M54.027 187.23c-.608.564-1.797.302-2.604-.589-.834-.889-.99-2.077-.373-2.65.627-.563 1.78-.3 2.616.59.834.899.996 2.08.36 2.65M58.33 194.39c-.782.543-2.06.034-2.849-1.1-.781-1.133-.781-2.493.017-3.038.792-.545 2.05-.055 2.85 1.07.78 1.153.78 2.513-.019 3.069M65.606 202.683c-.699.77-2.187.564-3.277-.488-1.114-1.028-1.425-2.487-.724-3.258.707-.772 2.204-.555 3.302.488 1.107 1.026 1.445 2.496.7 3.258M75.01 205.483c-.307.998-1.741 1.452-3.185 1.028-1.442-.437-2.386-1.607-2.095-2.616.3-1.005 1.74-1.478 3.195-1.024 1.44.435 2.386 1.596 2.086 2.612M85.714 206.67c.036 1.052-1.189 1.924-2.705 1.943-1.525.033-2.758-.818-2.774-1.852 0-1.062 1.197-1.926 2.721-1.951 1.516-.03 2.758.815 2.758 1.86M96.228 206.267c.182 1.026-.872 2.08-2.377 2.36-1.48.27-2.85-.363-3.039-1.38-.184-1.052.89-2.105 2.367-2.378 1.508-.262 2.857.355 3.049 1.398" />
+      </g>
+    </svg>
+    <ul class="">
+      <li class="">
+        <a href="https://github.com/abdigunasetiawan" class=" flex gap-1">
+          <span class="text-gray-800 underline text-sm dark:text-slate-400">Abdi's Github</span>
+          <svg class="size-4 dark:stroke-slate-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-external-link">
+            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+            <path d="M12 6h-6a2 2 0 0 0 -2 2v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-6" />
+            <path d="M11 13l9 -9" />
+            <path d="M15 4h5v5" />
+          </svg>
+        </a>
+      </li>
+      <li class="">
+        <a href="https://github.com/fachrizal02" class=" flex gap-1">
+          <span class="text-gray-800 underline text-sm dark:text-slate-400">Fachrizal's Github</span>
+          <svg class="size-4 dark:stroke-slate-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-external-link">
+            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+            <path d="M12 6h-6a2 2 0 0 0 -2 2v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-6" />
+            <path d="M11 13l9 -9" />
+            <path d="M15 4h5v5" />
+          </svg>
+        </a>
+      </li>
+    </ul>
+  </div>
+</div>
+
+<hr class="dark:boder dark:border-slate-700 mt-4 mb-6" />
+
+<div class="flex gap-6 flex-col md:flex-row flex-wrap">
+  <!-- Praktik P1 -->
+  <div class="p-5 dark:bg-slate-800 shadow-md rounded-lg md:w-42">
+    <span class="text-xs font-medium text-slate-400">Praktik</span>
+    <h3 class="font-bold text-slate-700 dark:text-slate-200">Pertemuan 1</h3>
+    <span class="text-xs font-medium text-slate-600 dark:text-slate-400">Kamis, 29 Februari 2024</span>
+    <hr class="dark:boder dark:border-slate-700 mt-2 mb-4" />
+    <ol>
+      <li><a class="transition underline text-sm text-gray-600 dark:text-slate-400 hover:text-gray-950" href="pertemuan1/latheading.html">latheading.html</a></li>
+      <li><a class="transition underline text-sm text-gray-600 dark:text-slate-400 hover:text-gray-950" href="pertemuan1/latparagraf.html">latparagraf.html</a></li>
+      <li><a class="transition underline text-sm text-gray-600 dark:text-slate-400 hover:text-gray-950" href="pertemuan1/lattulisan.html">lattulisan.html</a></li>
+    </ol>
+  </div>
+
+  <!-- Teori P1 -->
+  <div class="p-5 dark:bg-slate-800 shadow-md rounded-lg md:w-42">
+    <span class="text-xs font-medium text-slate-400">Teori</span>
+    <h3 class="font-bold text-slate-700 dark:text-slate-200">Pertemuan 1</h3>
+    <span class="text-xs font-medium text-slate-600 dark:text-slate-400">Kamis, 29 Februari 2024</span>
+    <hr class="dark:boder dark:border-slate-700 mt-2 mb-4" />
+    <div class="flex gap-2 flex-wrap mb-2">
+      <a href="" data-src="pertemuan1/tugas-teori1.jpg" class="size-14 md:size-16 lg:size-18 block hover:opacity-75 transition rounded-sm overflow-hidden group" data-fancybox="gallery">
+        <img class="w-full h-full group-hover:scale-110 transition duration-100" src="pertemuan1/tugas-teori1-thumb.jpg" alt="" />
+      </a>
+      <a href="" data-src="pertemuan1/tugas-teori2.jpg" class="size-14 md:size-16 lg:size-18 block hover:opacity-75 transition rounded-sm overflow-hidden group" data-fancybox="gallery">
+        <img class="w-full h-full group-hover:scale-110 transition duration-100" src="pertemuan1/tugas-teori2-thumb.jpg" alt="" />
+      </a>
+      <a href="" data-src="pertemuan1/tugas-teori3.jpg" class="size-14 md:size-16 lg:size-18 block hover:opacity-75 transition rounded-sm overflow-hidden group" data-fancybox="gallery">
+        <img class="w-full h-full group-hover:scale-110 transition duration-100" src="pertemuan1/tugas-teori3-thumb.jpg" alt="" />
+      </a>
+      <a href="" data-src="pertemuan1/tugas-teori4.jpg" class="size-14 md:size-16 lg:size-18 block hover:opacity-75 transition rounded-sm overflow-hidden group" data-fancybox="gallery">
+        <img class="w-full h-full group-hover:scale-110 transition duration-100" src="pertemuan1/tugas-teori4-thumb.jpg" alt="" />
+      </a>
+      <a href="" data-src="pertemuan1/tugas-teori5.jpg" class="size-14 md:size-16 lg:size-18 block hover:opacity-75 transition rounded-sm overflow-hidden group" data-fancybox="gallery">
+        <img class="w-full h-full group-hover:scale-110 transition duration-100" src="pertemuan1/tugas-teori5-thumb.jpg" alt="" />
+      </a>
+    </div>
+    <ol>
+      <li><a class="transition underline text-sm text-gray-600 dark:text-slate-400 hover:text-gray-950" href="pertemuan1/tugas-teori1-desain-web-Abdi_Guna_Setiawan-23441001_SI_A.pdf">tugas-teori-abdi.pdf</a></li>
+      <li><a class="transition underline text-sm text-gray-600 dark:text-slate-400 hover:text-gray-950" href="pertemuan1/tugas-teori1-desain-web- Fachrizal_Hadiansyah_ 23441186_SI_A.pdf">tugas-teori-fachrizal.pdf</a></li>
+    </ol>
+  </div>
+
+  <!-- Praktik P2 -->
+  <div class="p-5 dark:bg-slate-800 shadow-md rounded-lg md:w-42">
+    <span class="text-xs font-medium text-slate-400">Praktik</span>
+    <h3 class="font-bold text-slate-700 dark:text-slate-200">Pertemuan 2</h3>
+    <span class="text-xs font-medium text-slate-600 dark:text-slate-400">Kamis, 7 Maret 2024</span>
+    <hr class="dark:boder dark:border-slate-700 mt-2 mb-4" />
+    <ol>
+      <li><a class="transition underline text-sm text-gray-600 dark:text-slate-400 hover:text-gray-950" href="pertemuan2/latfont.html">latfont.html</a></li>
+      <li><a class="transition underline text-sm text-gray-600 dark:text-slate-400 hover:text-gray-950" href="pertemuan2/latlink.html">latlink.html</a></li>
+      <li><a class="transition underline text-sm text-gray-600 dark:text-slate-400 hover:text-gray-950" href="pertemuan2/latpenomoran.html">latpenomoran.html</a></li>
+      <li><a class="transition underline text-sm text-gray-600 dark:text-slate-400 hover:text-gray-950" href="pertemuan2/latgambar.html">latgambar.html</a></li>
+      <li><a class="transition underline text-sm text-gray-600 dark:text-slate-400 hover:text-gray-950" href="pertemuan2/latlinkdangambar.html">latlinkdangambar.html</a></li>
+    </ol>
+  </div>
+
+  <!-- Praktik P3 -->
+  <div class="p-5 dark:bg-slate-800 shadow-md rounded-lg md:w-42">
+    <span class="text-xs font-medium text-slate-400">Praktik</span>
+    <h3 class="font-bold text-slate-700 dark:text-slate-200">Pertemuan 3</h3>
+    <span class="text-xs font-medium text-slate-600 dark:text-slate-400">Kamis, 14 Maret 2024</span>
+    <hr class="dark:boder dark:border-slate-700 mt-2 mb-4" />
+    <ol>
+      <li><a class="transition underline text-sm text-gray-600 dark:text-slate-400 hover:text-gray-950" href="pertemuan3/judul.html">judul.html</a></li>
+      <li><a class="transition underline text-sm text-gray-600 dark:text-slate-400 hover:text-gray-950" href="pertemuan3/menu.html">menu.html</a></li>
+      <li><a class="transition underline text-sm text-gray-600 dark:text-slate-400 hover:text-gray-950" href="pertemuan3/latframe.html">latframe.html</a></li>
+      <li><a class="transition underline text-sm text-gray-600 dark:text-slate-400 hover:text-gray-950" href="pertemuan3/lattable.html">lattable.html</a></li>
+    </ol>
+  </div>
+
+  <!-- Praktik P4 -->
+  <div class="p-5 dark:bg-slate-800 shadow-md rounded-lg md:w-42">
+    <span class="text-xs font-medium text-slate-400">Praktik</span>
+    <h3 class="font-bold text-slate-700 dark:text-slate-200">Pertemuan 4</h3>
+    <span class="text-xs font-medium text-slate-600 dark:text-slate-400">Kamis, 21 Maret 2024</span>
+    <hr class="dark:boder dark:border-slate-700 mt-2 mb-4" />
+    <ol>
+      <li><a class="transition underline text-sm text-gray-600 dark:text-slate-400 hover:text-gray-950" href="pertemuan4/latform.html">latform.html</a></li>
+      <li><a class="transition underline text-sm text-gray-600 dark:text-slate-400 hover:text-gray-950" href="pertemuan4/daftar.html">daftar.html</a></li>
+    </ol>
+  </div>
+</div>
+</div>
+
+<div class="fixed right-4 bottom-4 flex items-center gap-4">
+<div id="themeToggler" class="p-3 cursor-pointer bg-slate-700 rounded-full shadow-lg active:outline active:outline-slate-950" >
+  
+</div>
+<button onClick="logout()" id="logout" class="bg-red-600 dark:bg-red-800 dark:hover:bg-red-900 active:outline active:outline-red-950  hover:bg-red-700 flex gap-2 text-white font-medium px-4 py-2 rounded-lg shadow-2xl ">
+  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-logout">
+    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+    <path d="M14 8v-2a2 2 0 0 0 -2 -2h-7a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h7a2 2 0 0 0 2 -2v-2" />
+    <path d="M9 12h12l-3 -3" />
+    <path d="M18 15l3 -3" />
+  </svg>
+  <span>Logout</span>
+</button>
+
+</div>
+<!-- Javascript -->
+<script type="module">
+import { Fancybox } from 'https://cdn.jsdelivr.net/npm/@fancyapps/ui@5.0/dist/fancybox/fancybox.esm.js';
+
+Fancybox.bind('[data-fancybox="gallery"]', {});
+</script>`;
+const containerElement = document.createElement('div');
+containerElement.innerHTML = containerContent;
+
+function logout() {
+  const themeToggler = document.querySelector('#themeToggler');
+  const logoutButton = document.querySelector('button#logout');
+  window.location.reload();
+  console.log('clicked');
+  localStorage.setItem('showOnBoarding', true);
+}
+
+function theme() {
+  const themeToggler = document.querySelector('#themeToggler');
+
+  if (localStorage.theme == 'dark') {
+    themeToggler.innerHTML = `<svg class="stoke stroke-white" xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"    stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-sun"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 12m-4 0a4 4 0 1 0 8 0a4 4 0 1 0 -8 0" /><path d="M3 12h1m8 -9v1m8 8h1m-9 8v1m-6.4 -15.4l.7 .7m12.1 -.7l-.7 .7m0 11.4l.7 .7m-12.1 -.7l-.7 .7" /></svg>`;
+  } else {
+    themeToggler.innerHTML = `<svg class="stroke stroke-white"  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"   stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-moon"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 3c.132 0 .263 0 .393 0a7.5 7.5 0 0 0 7.92 12.446a9 9 0 1 1 -8.313 -12.454z" /></svg>`;
+  }
+
+  themeToggler.addEventListener('click', () => {
+    const html = document.querySelector('html');
+    const themeToggler = document.querySelector('#themeToggler');
+
+    if (localStorage.theme == 'dark') {
+      html.classList.remove('dark');
+      html.classList.add('light');
+      localStorage.theme = 'light';
+      themeToggler.innerHTML = `<svg class="stroke stroke-white"  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"   stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-moon"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 3c.132 0 .263 0 .393 0a7.5 7.5 0 0 0 7.92 12.446a9 9 0 1 1 -8.313 -12.454z" /></svg>`;
+    } else {
+      html.classList.add('dark');
+      html.classList.remove('light');
+      localStorage.theme = 'dark';
+      themeToggler.innerHTML = `<svg class="stoke stroke-white" xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"    stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-sun"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 12m-4 0a4 4 0 1 0 8 0a4 4 0 1 0 -8 0" /><path d="M3 12h1m8 -9v1m8 8h1m-9 8v1m-6.4 -15.4l.7 .7m12.1 -.7l-.7 .7m0 11.4l.7 .7m-12.1 -.7l-.7 .7" /></svg>`;
+    }
+  });
+}
+
+function onBoardingHandler() {
+  const openButton = document.querySelector('button#open');
+  const onboarding = document.querySelector('#onboarding');
+
+  if (localStorage.getItem('showOnBoarding') == 'false') {
+    onboarding.classList.add('hidden');
+    document.body.appendChild(containerElement);
+    localStorage.setItem('theme', 'light');
+    theme();
+  } else if (localStorage.getItem('showOnBoarding') == 'true') {
+    onboarding.classList.add('flex');
+    onboarding.classList.remove('hidden');
+    openButton.addEventListener('click', () => {
+      document.body.appendChild(containerElement);
+      onboarding.classList.add('open');
+      localStorage.setItem('showOnBoarding', false);
+      theme();
+    });
+  }
+}
+
+window.addEventListener('keydown', e => {
+  if (e.key == 'a') {
+    document.body.classList.toggle('debug');
+  }
+});
+
+// On page load or when changing themes, best to add inline in `head` to avoid FOUC
+if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+  document.documentElement.classList.add('dark');
+} else {
+  document.documentElement.classList.remove('dark');
+}
